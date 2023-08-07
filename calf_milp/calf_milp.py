fe88bf36997af801c2f25ec3aec12b53535aa2f5
rolf <hrolfrc@gmail.com>
2023-08-07 10:56:33 -0600

    updated examples

--- calf_milp/calf_milp.py
+++ calf_milp/calf_milp.py
@@ -254,7 +254,6 @@ class CalfMilp(ClassifierMixin, BaseEstimator):
         Examples
         --------
             >>> import numpy
-            >>> from calfcv import Calf
             >>> from sklearn.datasets import make_classification as mc
             >>> X, y = mc(n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1, random_state=42)
             >>> numpy.round(X[0:3, :], 2)
